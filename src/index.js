7f2180f4b5cabf42f02d5c705999e659df204e42
onesmus kamara <onesmusqamara@gmail.com>
2023-03-31 08:07:07 +0300

    improved javascript

--- src/index.js
+++ src/index.js
@@ -1,49 +1,31 @@
-// Get the latest news articles
-const latestArticles = document.querySelectorAll('main section article');
-
-// Add click event listener to each article
-latestArticles.forEach(article => {
-  article.addEventListener('click', () => {
-    // Get the article title and content
-    const title = article.querySelector('h3').textContent;
-    const content = article.querySelector('p').textContent;
-    
-    // Display the article in a modal
-    showModal(title, content);
-  });
+ // Add the latest news articles to the page
+ articles.forEach(article => {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  link.textContent = article.title;
+  link.setAttribute('href', article.url);
+  listItem.appendChild(link);
+  latestNewsList.appendChild(listItem);
 });
 
-// Function to display a modal with the article content
-function showModal(title, content) {
-  // Create modal elements
-  const modal = document.createElement('div');
-  const modalContent = document.createElement('div');
-  const modalTitle = document.createElement('h3');
-  const modalClose = document.createElement('button');
-  const modalBody = document.createElement('p');
-  
-  // Add content to modal elements
-  modalTitle.textContent = title;
-  modalClose.textContent = 'Close';
-  modalBody.textContent = content;
-  
-  // Add classes to modal elements
-  modal.classList.add('modal');
-  modalContent.classList.add('modal-content');
-  modalTitle.classList.add('modal-title');
-  modalClose.classList.add('modal-close');
-  modalBody.classList.add('modal-body');
-  
-  // Append modal elements to the page
-  modalContent.appendChild(modalTitle);
-  modalContent.appendChild(modalClose);
-  modalContent.appendChild(modalBody);
-  modal.appendChild(modalContent);
-  document.body.appendChild(modal);
-  
-  // Add click event listener to close button
-  modalClose.addEventListener('click', () => {
-    document.body.removeChild(modal);
-  });
-}
+// Add a click event listener to the featured article to display more information
+const featuredArticle = document.querySelector('#featured-news article');
+featuredArticle.addEventListener('click', event => {
+  const articleSummary = event.currentTarget.querySelector('p');
+  const articleUrl = event.currentTarget.querySelector('a');
+  articleSummary.style.display = 'block';
+  articleUrl.style.display = 'block';
+});
 
+
+
+// Add a click event listener to the navigation menu to highlight the selected page
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+link.addEventListener('click', event => {
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+  });
+  event.currentTarget.classList.add('active');
+});
+});
